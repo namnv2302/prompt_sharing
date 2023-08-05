046aa3e7a4f0ab2455fe8c85195db93180a7e95c
namnv <namnv2302@gmail.com>
2023-08-05 18:15:19 +0700

    fix type akay

--- utils/database.ts
+++ utils/database.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 
 let isConnected: boolean = false;
 
@@ -9,11 +9,11 @@ export const connectToDB = async () => {
     return;
   }
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI || "", {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
+    } as ConnectOptions);
     isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
